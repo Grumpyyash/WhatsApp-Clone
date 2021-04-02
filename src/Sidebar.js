91f207bb67d375f889927c0e4a261af7458a93d1
Yash Vardhan <yashvardhan513@gmail.com>
2021-04-02 17:07:12 +0530

    Initial commit

---
+++ src/Sidebar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Sidebar.css"
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+          <div className="sidebar__header">
+              <div className="sidebar__headerRight">
+
+              </div>
+          </div>
+        </div>
+    )
+}
+
+export default Sidebar
+
